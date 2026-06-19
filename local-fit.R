860b97a87bdc6fc19c3e54b72d671169b3e47fe8
Patrick Maher <86903278+pmaher1@users.noreply.github.com>
2026-06-19 11:58:32 +1000

    Update local-fit.R

--- local-fit.R
+++ local-fit.R
@@ -19,7 +19,7 @@ local_result <- fit_one_rep(
   beta_0 = 73, beta_1 = -0.04, beta_2 = 0.04, sigma_e = 12,
   log_AF = -0.90, alpha_AFT = 0.012,
   loglogistic_shape = 1.20, loglogistic_scale = 23,
-  visit = c(0, 1, seq(3, 92, 3)), n_patients = 300, max_FU = 120,
+  visit = c(0, 1, seq(3, 92, 3)), n_patients = 100, max_FU = 120,
   lambda_c = -1, aft_mode = "loglogistic",
   models_to_fit = c("LMM", "bp2", "gp"),
   n_stan_chains = 4, n_stan_warmup = 1000, n_stan_iter = 1000
@@ -29,9 +29,9 @@ local_result <- fit_one_rep(
 format_summary <- function(summary_tbl, y_mean = 0, model = c("bp", "gp")) {
   model <- match.arg(model)
   if (is.null(summary_tbl)) return(NULL)
-
+  
   surv_var <- if (model == "gp") "gamma[1]" else "beta_surv[1]"
-
+  
   summary_tbl %>%
     filter(variable %in% c(
       "beta_long[1]", "beta_long[2]", "beta_long[3]",
@@ -42,9 +42,9 @@ format_summary <- function(summary_tbl, y_mean = 0, model = c("bp", "gp")) {
         variable == "beta_long[1]" ~ "beta_0",
         variable == "beta_long[2]" ~ "beta_1",
         variable == "beta_long[3]" ~ "beta_2",
-        variable == surv_var ~ "gamma",
-        variable == "alpha" ~ "alpha",
-        TRUE ~ variable
+        variable == surv_var       ~ "gamma",
+        variable == "alpha"        ~ "alpha",
+        TRUE                       ~ variable
       ),
       mean = if_else(variable == "beta_long[1]", mean + y_mean, mean),
       q2.5 = if_else(variable == "beta_long[1]", q2.5 + y_mean, q2.5),
@@ -62,7 +62,7 @@ lmm_summary <- local_result$LMM$summary %>%
          mean = if_else(variable == "beta_long[1]", mean + local_result$Y_long_mean, mean), # Adding back the longitudinal mean from centring
          q2.5 = if_else(variable == "beta_long[1]", q2.5 + local_result$Y_long_mean, q2.5),
          q97.5 = if_else(variable == "beta_long[1]", q97.5 + local_result$Y_long_mean, q97.5),
-         sd = NA_real_) %>%
+         sd = if ("sd" %in% names(.)) sd else (q97.5 - q2.5) / (2 * 1.96)) %>%
   dplyr::select(parameter, mean, sd, q2.5, q97.5)
 
 # Direct comparison of results of models
